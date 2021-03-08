4f17f6adc7ba0523db6f3da3ac9790de505ec5f4
mantivo <ovitnam@gmail.com>
2021-03-08 16:02:10 -0500

    030821 - Added pagination

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/*
+    Author: Alex Doe
+    Date: 03/06/2021
+    File Name: scripts.js
+*/
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+
+
+function erase() {
+    document.getElementById("everything").style.display = "none";
+}
